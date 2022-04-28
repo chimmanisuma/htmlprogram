1dcc2f5f475fbdb7ad6fa91fec2eb8928ca33150
chimmanisuma <chimmanisuma014@gmail.com>
2022-04-28 15:25:08 +0530

    htmlprogram

---
+++ index.js
@@ -0,0 +1,10 @@
+function seterror(id, error){
+    //set error inside tag of id
+}
+
+function validateForm(){
+    var name = document.forms['myForm']["fname"].value;
+    console.log(name);
+    return false;
+
+}
